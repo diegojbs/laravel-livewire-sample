88d63f4460d116ed77bc0a2a234cd3d993467c0a
Diego Suarez <diego-jbs@hotmail.com>
2024-10-22 20:21:24 -0500

    CRUD usuarios con modal

--- app/Livewire/UsuariosComponent.php
+++ app/Livewire/UsuariosComponent.php
@@ -4,31 +4,77 @@ namespace App\Livewire;
 
 use App\Models\User;
 use Livewire\Component;
+use Livewire\Attributes\Url;
+use Livewire\WithPagination;
+use Livewire\Attributes\On; 
 
 class UsuariosComponent extends Component
 {
+    use WithPagination;
+
     public $name;
     public $email;
+    public $mensaje = '';
+    public $showModal = false;
+    public $idItem = null;
+
+    #[Url(as: 'q')]
+    public $search = '';
 
     public function mount(){
         $this->name = '';
         $this->email = '';
     }
 
+    #[On('user-created')] 
+    public function userCreatedListener(){
+        $this->mensaje = 'Usuario creado correctamente';
+    }
+
+    public function setItem($id){
+        $this->idItem = $id;
+        $item = User::find($id);
+        $this->name = $item->name;
+        $this->email = $item->email;
+        $this->showModal = true;
+    }
+
     public function guardar(){
 
-        // dd($this->name, $this->email);
-        $item = new User();
-        $item->name = $this->name;
-        $item->email = $this->email;
-        $item->save();
-
-        $this->reset(['name', 'email']);
+        try {
+            //code...
+            sleep(3);
+            if ($this->idItem != null) {
+                $item = User::find($this->idItem);
+                $item->name = $this->name;
+                $item->email = $this->email;
+                $item->save();
+                $this->mensaje = 'Usuario actualizado correctamente';
+            }else{
+                $item = new User();
+                $item->name = $this->name;
+                $item->email = $this->email;
+                $item->save();
+            }
+            // dd($this->name, $this->email);
+    
+            $this->reset(['name', 'email', 'showModal', 'idItem']);
+    
+            $this->dispatch('user-created');
+        } catch (\Throwable $th) {
+            $this->mensaje = 'Error al crear el usuario' . $th->getMessage();
+        }
     }
 
     public function render()
     {
-        $data = User::all();
+        if($this->search != ''){
+            $data = User::where('name', 'ilike', '%'.$this->search.'%')
+                ->orWhere('email', 'ilike', '%'.$this->search.'%')            
+                ->paginate(10);
+        }else{
+            $data = User::paginate(10);
+        }
         return view('livewire.usuarios-component', compact('data'));
     }
 }
--- resources/views/livewire/usuarios-component.blade.php
+++ resources/views/livewire/usuarios-component.blade.php
@@ -1,21 +1,27 @@
-<div>
-    {{-- <h1>{{ $count }}</h1>
- 
-    <button wire:click="increment">+</button>
- 
-    <button wire:click="decrement">-</button> --}}
+<div  x-data="{ showModal: $wire.entangle('showModal').live }">
+    @if ($mensaje != '')
+    <div class="div">
+      <div class="alert alert-warning">
+        {{ $mensaje }}
+      </div>
+    </div>
+    @endif
+
+    <button class="btn btn-primary" x-on:click="showModal = true">Nuevo</button>
+
+
+    {{$showModal}}
 
     <div>
-        <div class="form-group">
-          <label for="exampleInputName">Name</label>
-          <input type="text" class="form-control" id="exampleInputName" aria-describedby="emailHelp" wire:model="name">
-        </div>
-        <div class="form-group">
-            <label for="exampleInputEmail">Email</label>
-            <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp" wire:model="email">
-          </div>
-        <button class="btn btn-primary" wire:click="guardar()">Guardar</button>
+      <input class="form-control" placeholder="Buscar..." type="text" wire:model.live="search">
     </div>
+
+    {{-- <div>
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal"
+        x-on:click="showModal = true">
+        Launch demo modal
+      </button>
+    </div> --}}
 
     {{-- Lista de usaurios --}}
 
@@ -25,7 +31,7 @@
             <th scope="col">ID</th>
             <th scope="col">Name</th>
             <th scope="col">Email</th>
-            <th scope="col">Handle</th>
+            <th scope="col">Acciones</th>
           </tr>
         </thead>
         <tbody>
@@ -34,13 +40,59 @@
                 <th scope="row">{{$item->id}}</th>
                 <td>{{$item->name}}</td>
                 <td>{{$item->email}}</td>
-                {{-- <td>@fat</td> --}}
+                <td>
+                  <div class="btn btn-warning" wire:click="setItem({{$item->id}})">Editar</div>
+                </td>
             </tr>
           @endforeach
           
         </tbody>
       </table>
 
+      <div>
+        {{ $data->links() }}
+      </div>
+
     {{-- Lista de usuarios FIn| --}}
+
+    <!-- Modal -->
+    <div x-bind:class="! showModal ? 'modal fade' : 'modal fade show'" 
+          x-bind:aria-hidden="! showModal ? 'true' : 'false'" 
+          x-bind:style="! showModal ? '' : 'display: block;'" 
+          x-bind:role="! showModal ? '' : 'dialog'">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            {{-- Cuerpo del modal --}}
+            <div class="form-group">
+              <label for="exampleInputName">Name</label>
+              <input type="text" class="form-control" id="exampleInputName" aria-describedby="emailHelp" wire:model="name">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail">Email</label>
+                <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp" wire:model="email">
+              </div>
+              
+              <div class="mt-3">
+                <div wire:loading class="spinner-grow text-info"> 
+                  <span class="sr-only"></span>
+                </div>
+              </div>
+            {{-- Fin cuerpo del modal --}}
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal" x-on:click="showModal = false">Close</button>
+            <button type="button" class="btn btn-primary" wire:click="guardar()">Guardar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    {{-- Fin modal --}}
 
 </div>
